98f8648218109c9efd6fd543a257ae46177c6a7e
hitesh <hiteshvpatel256@gmail.com>
2021-10-22 14:29:15 +0530

    [B180-394] Removing non utilized files after merge ProtocolBroker into ProtocolAggregator

--- protocolAggregator/aggregator-commons/src/main/java/com/protocol180/aggregator/commons/PostOfficeMapKey.kt
+++
@@ -1,9 +0,0 @@
-package com.protocol180.aggregator.commons
-
-import java.security.PrivateKey
-import java.security.PublicKey
-
-data class PostOfficeMapKey(
-    val senderPrivateKey: PrivateKey,
-    val recipientPublicKey: PublicKey,
-    val topic: String)
