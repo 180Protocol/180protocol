9c4e14cb58eef821e4ef68463e01d552bac88c96
hitesh <hiteshvpatel256@gmail.com>
2021-11-11 23:03:42 +0530

    [B180-403] Updating all states and skeleton contract into kotlin version.

---
+++ protocolAggregator/contracts/src/main/kotlin/com/protocol180/aggregator/states/ProviderAggregationState.kt
@@ -0,0 +1,28 @@
+package com.protocol180.aggregator.states
+
+import com.protocol180.aggregator.contracts.AggregationContract
+import com.protocol180.commons.SchemaType
+import net.corda.core.contracts.BelongsToContract
+import net.corda.core.contracts.ContractState
+import net.corda.core.contracts.StaticPointer
+import net.corda.core.identity.AbstractParty
+import net.corda.core.identity.AnonymousParty
+import net.corda.core.identity.Party
+
+@BelongsToContract(AggregationContract::class)
+data class ProviderAggregationState(val consumer: AnonymousParty,
+                                    val host: Party,
+                                    val failedReason:String,
+                                    val pointedToProviderAggregationState: StaticPointer<ProviderAggregationState>,
+                                    val pointedToDataOutputState: StaticPointer<DataOutputState>,
+                                    val dataType: SchemaType) : ContractState {
+
+    /**
+     *  This property holds a list of the nodes which can "use" this state in a valid transaction. In this case, the
+     *  consumer or host.
+     */
+    override val participants: List<AbstractParty> get() = listOf(consumer, host)
+
+
+
+}
