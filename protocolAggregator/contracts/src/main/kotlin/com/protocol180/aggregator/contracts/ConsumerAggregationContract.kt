455da92d32f87660cf80e2af649e06d53edc1cc6
hitesh <hiteshvpatel256@gmail.com>
2021-11-14 10:13:20 +0530

    [B180-413] Implementation of ConsumerAggregationFlow for Propose command. Contracts has been added with constraints for Propose.

---
+++ protocolAggregator/contracts/src/main/kotlin/com/protocol180/aggregator/contracts/ConsumerAggregationContract.kt
@@ -0,0 +1,48 @@
+package com.protocol180.aggregator.contracts
+
+import com.protocol180.aggregator.states.ConsumerAggregationState
+import net.corda.core.contracts.*
+import net.corda.core.transactions.LedgerTransaction
+
+class ConsumerAggregationContract : Contract {
+    companion object {
+        @JvmStatic
+        val Aggregation_Propose_CONTRACT_ID = "com.protocol180.aggregator.contracts.AggregationContract"
+    }
+
+    /**
+     * Takes an object that represents a state transition, and ensures the inputs/outputs/commands make sense.
+     * Must throw an exception if there's a problem that should prevent state transition. Takes a single object
+     * rather than an argument so that additional data can be added without breaking binary compatibility with
+     * existing contract code.
+     */
+    override fun verify(tx: LedgerTransaction) {
+        val command = tx.commands.requireSingleCommand<Commands>()
+
+        when (command.value) {
+            is Commands.Propose -> requireThat {
+                "No inputs should be consumed when issuing an Propose." using (tx.inputs.isEmpty())
+                "Only one output state should be created when issuing an Propose." using (tx.outputs.size == 1)
+                val consumerAggregationState = tx.outputsOfType<ConsumerAggregationState>().single()
+                "A newly issued Propose must have Consumer & Host Public key" using
+                        (consumerAggregationState.consumer != null && consumerAggregationState.host != null)
+                "The Type of Data required from consumer should not be empty" using (consumerAggregationState.dataType != null)
+                "List of Pointers to provider aggregation states & Data Output state must not been initialized at this stage " using
+                        (consumerAggregationState.pointedToProviderAggregationState == null && consumerAggregationState.pointedToDataOutputState == null)
+                "Both consumer and host together only may sign ConsumerAggregation Propose Transaction." using
+                        (command.signers.toSet() == consumerAggregationState.participants.map { it.owningKey }.toSet())
+            }
+
+        }
+    }
+
+    /**
+     * Add any commands required for this contract as classes within this interface.
+     * It is useful to encapsulate your commands inside an interface, so you can use the [requireSingleCommand]
+     * function to check for a number of commands which implement this interface.
+     */
+    interface Commands : CommandData {
+
+        class Propose : TypeOnlyCommandData(), Commands
+    }
+}
---
+++ protocolAggregator/workflows/src/main/kotlin/com/protocol180/cordapp/aggregation/flow/ConsumerAggregationProposeFlow.kt
@@ -0,0 +1,77 @@
+package com.protocol180.cordapp.aggregation.flow
+
+import co.paralleluniverse.fibers.Suspendable
+import com.protocol180.aggregator.contracts.ConsumerAggregationContract
+import com.protocol180.aggregator.states.ConsumerAggregationState
+import net.corda.core.contracts.Command
+import net.corda.core.contracts.requireThat
+import net.corda.core.flows.*
+import net.corda.core.transactions.SignedTransaction
+import net.corda.core.transactions.TransactionBuilder
+
+/**
+ * This is the flow which handles issuance of new Aggregation Request .
+ * Providing the information related to different provider required to be participated in the flow .
+ * The flow returns the [SignedTransaction] that was committed to the ledger.
+ */
+@InitiatingFlow
+@StartableByRPC
+class ConsumerAggregationProposeFlow(val state: ConsumerAggregationState): FlowLogic<SignedTransaction>() {
+    @Suspendable
+    override fun call(): SignedTransaction {
+
+        // Step 1. Get a reference to the notary service on our network and our key pair.
+        // Note: ongoing work to support multiple notary identities is still in progress.
+
+        /**
+         *  METHOD 1: Take first notary on network, WARNING: use for test, non-prod environments, and single-notary networks only!*
+         *  METHOD 2: Explicit selection of notary by CordaX500Name - argument can by coded in flow or parsed from config (Preferred)
+         *
+         *  * - For production you always want to use Method 2 as it guarantees the expected notary is returned.
+         */
+        val notary = serviceHub.networkMapCache.notaryIdentities.single() // METHOD 1
+        // val notary = serviceHub.networkMapCache.getNotary(CordaX500Name.parse("O=Notary,L=London,C=GB")) // METHOD 2
+
+        // Step 2. Create a new issue command.
+        // Remember that a command is a CommandData object and a list of CompositeKeys
+        val issueCommand = Command(ConsumerAggregationContract.Commands.Propose(), state.participants.map { it.owningKey })
+
+        // Step 3. Create a new TransactionBuilder object.
+        val builder = TransactionBuilder(notary = notary)
+
+        // Step 4. Add the ConsumerAggregationState as an output state, as well as a command to the transaction builder.
+        builder.addOutputState(state, ConsumerAggregationContract.Aggregation_Propose_CONTRACT_ID)
+        builder.addCommand(issueCommand)
+
+        // Step 5. Verify and sign it with our KeyPair.
+        builder.verify(serviceHub)
+        val ptx = serviceHub.signInitialTransaction(builder)
+
+        val sessions = (state.participants - ourIdentity).map { initiateFlow(it) }.toSet()
+        // Step 6. Collect the other party's signature using the SignTransactionFlow.
+        val stx = subFlow(CollectSignaturesFlow(ptx, sessions))
+
+        // Step 7. Assuming no exceptions, we can now finalise the transaction.
+        return subFlow(FinalityFlow(stx, sessions))
+    }
+}
+
+/**
+ * This is the flow which signs IOU issuances.
+ * The signing is handled by the [SignTransactionFlow].
+ */
+@InitiatedBy(ConsumerAggregationProposeFlow::class)
+class AggregationProposeFlowResponder(val flowSession: FlowSession): FlowLogic<SignedTransaction>() {
+
+    @Suspendable
+    override fun call(): SignedTransaction {
+        val signedTransactionFlow = object : SignTransactionFlow(flowSession) {
+            override fun checkTransaction(stx: SignedTransaction) = requireThat {
+                val output = stx.tx.outputs.single().data
+                "This must initiate Provider Aggregation Flow" using (output is ConsumerAggregationState)
+            }
+        }
+        val txWeJustSignedId = subFlow(signedTransactionFlow)
+        return subFlow(ReceiveFinalityFlow(otherSideSession = flowSession, expectedTxId = txWeJustSignedId.id))
+    }
+}
