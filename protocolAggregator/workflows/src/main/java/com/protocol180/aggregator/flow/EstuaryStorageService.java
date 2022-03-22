7d84b20ab5b30acce729e3733a07b15a0bf9f808
Vivek Shah <vivekkshah7@gmail.com>
2022-03-22 20:23:26 +0530

    #46: Add estuary storage service and its test case.

---
+++ protocolAggregator/workflows/src/main/java/com/protocol180/aggregator/flow/EstuaryStorageService.java
@@ -0,0 +1,36 @@
+package com.protocol180.aggregator.flow;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.JSONArray;
+
+import java.io.File;
+
+public class EstuaryStorageService {
+    public String uploadContent(File file, String token) throws UnirestException {
+        HttpResponse<JsonNode> jsonResponse = Unirest.post("https://shuttle-4.estuary.tech/content/add")
+                .header("authorization", "Bearer " + token)
+                .field("data", file)
+                .asJson();
+
+        return jsonResponse.getBody().getObject().get("cid").toString();
+    }
+
+    public JSONArray fetchContent(String token) throws UnirestException {
+        HttpResponse<JsonNode> jsonResponse = Unirest.get("https://api.estuary.tech/content/list")
+                .header("authorization", "Bearer " + token)
+                .asJson();
+
+        return jsonResponse.getBody().getArray();
+    }
+
+    public JSONArray fetchContentByCid(String token, String cid) throws UnirestException {
+        HttpResponse<JsonNode> jsonResponse = Unirest.get("https://api.estuary.tech/content/by-cid/" + cid)
+                .header("authorization", "Bearer " + token)
+                .asJson();
+
+        return jsonResponse.getBody().getArray();
+    }
+}
