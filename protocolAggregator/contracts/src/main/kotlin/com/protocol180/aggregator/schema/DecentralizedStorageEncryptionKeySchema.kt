a6d90dc8e635c3491b81aca3acbbda47765bcde5
Vivek Shah <vivekkshah7@gmail.com>
2022-04-06 18:23:40 +0530

    #46: Store decentralized storage encryption key into db.

---
+++ protocolAggregator/contracts/src/main/kotlin/com/protocol180/aggregator/schema/DecentralizedStorageEncryptionKeySchema.kt
@@ -0,0 +1,34 @@
+package com.protocol180.aggregator.schema
+
+import net.corda.core.schemas.MappedSchema
+import java.io.Serializable
+import java.time.Instant
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+import javax.persistence.*
+
+object DecentralizedStorageEncryptionKeySchema
+
+object DecentralizedStorageEncryptionKeySchemaV1 : MappedSchema(
+    schemaFamily = DecentralizedStorageEncryptionKeySchema.javaClass,
+    version = 1,
+    mappedTypes = listOf(EncryptionKeyStorage::class.java)
+) {
+    @Entity
+    @Table(name = "DECENTRALIZED_STORAGE_ENCRYPTION_KEY")
+    class EncryptionKeyStorage(
+        @Id
+        @Column(name = "flow_id", nullable = false, unique = true)
+        var flowId: String,
+        @Lob
+        @Column(name = "key", nullable = false)
+        val key: SecretKey?,
+        @Column(name = "ivParameterSpec", nullable = false)
+        val ivParameterSpec: ByteArray,
+        @Column(name = "dateCreated", nullable = false)
+        val dateCreated: Instant
+    ) : Serializable {
+        constructor() : this("", KeyGenerator.getInstance("AES").generateKey(), ByteArray(0), Instant.now())
+    }
+
+}
---
+++ protocolAggregator/workflows/src/main/kotlin/com/protocol180/aggregator/flow/DecentralizedStorageEncryptionKeyUpdateFlow.kt
@@ -0,0 +1,33 @@
+package com.protocol180.aggregator.flow
+
+import co.paralleluniverse.fibers.Suspendable
+import com.protocol180.aggregator.utils.AESUtil
+import net.corda.core.flows.FlowLogic
+import net.corda.core.flows.InitiatingFlow
+import net.corda.core.flows.StartableByRPC
+import net.corda.core.utilities.ProgressTracker
+
+
+/**
+ * DecentralizedStorageEncryptionKeyFlow allows storing secret key and ivParameterSpec into db.
+ */
+
+@InitiatingFlow
+@StartableByRPC
+class DecentralizedStorageEncryptionKeyUpdateFlow : FlowLogic<Unit>() {
+    override val progressTracker = ProgressTracker()
+
+    @Suspendable
+    override fun call() {
+        val decentralizedStorageEncryptionKeyService =
+            serviceHub.cordaService(DecentralizedStorageEncryptionKeyService::class.java)
+
+        val key = AESUtil.generateKey(256)
+        val ivParameterSpec = AESUtil.generateIv()
+        return decentralizedStorageEncryptionKeyService.addDecentralizedStorageEncryptionKeyWithFlowId(
+            this.runId.uuid.toString(),
+            key,
+            ivParameterSpec.iv
+        )
+    }
+}
