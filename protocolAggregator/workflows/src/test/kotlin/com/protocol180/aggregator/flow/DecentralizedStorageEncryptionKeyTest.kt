96533b69d66f798b608ab2f42c948cf92d711ac4
Vivek Shah <vivekkshah7@gmail.com>
2022-04-06 18:23:40 +0530

    #46: Store decentralized storage encryption key into db.

---
+++ protocolAggregator/workflows/src/test/kotlin/com/protocol180/aggregator/flow/DecentralizedStorageEncryptionKeyTest.kt
@@ -0,0 +1,61 @@
+package com.protocol180.aggregator.flow
+
+import com.protocol180.aggregator.states.RoleType
+import net.corda.testing.node.*
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class DecentralizedStorageEncryptionKeyTest {
+    lateinit var network: MockNetwork
+    lateinit var consumer: StartedMockNode
+
+    @Before
+    fun setup() {
+        network = MockNetwork(
+            MockNetworkParameters(
+                cordappsForAllNodes = listOf(
+                    TestCordapp.findCordapp("com.protocol180.aggregator.contracts"),
+                    TestCordapp.findCordapp("com.protocol180.aggregator.flow")
+                )
+            )
+        )
+        consumer = prepareNodeForRole(RoleType.DATA_CONSUMER)
+        network.runNetwork()
+    }
+
+    private fun prepareNodeForRole(role: RoleType): StartedMockNode {
+        return network.createNode(
+            parameters = MockNodeParameters(
+                additionalCordapps = listOf(
+                    TestCordapp.findCordapp("com.protocol180.aggregator.flow")
+                        .withConfig(mapOf(Pair(NetworkParticipantService.PARTICIPANT_ROLE_CONFIG_KEY, role.name)))
+                )
+            )
+        )
+    }
+
+
+    @After
+    fun tearDown() {
+        network.stopNodes()
+    }
+
+    @Test
+    fun decentralizedStorageEncryptionKeyTest() {
+        val flow1 = DecentralizedStorageEncryptionKeyUpdateFlow()
+        consumer.startFlow(flow1)
+        network.runNetwork()
+
+        val flow = DecentralizedStorageEncryptionKeyUpdateFlow()
+        consumer.startFlow(flow)
+        network.runNetwork()
+
+        val decentralizedStorageEncryptionKeyService =
+            consumer.services.cordaService(DecentralizedStorageEncryptionKeyService::class.java)
+        val outputRecords = decentralizedStorageEncryptionKeyService.retrieveLatestDecentralizedStorageEncryptionKey();
+        assertEquals(flow.runId.uuid.toString(), outputRecords?.flowId)
+        network.runNetwork()
+    }
+}
