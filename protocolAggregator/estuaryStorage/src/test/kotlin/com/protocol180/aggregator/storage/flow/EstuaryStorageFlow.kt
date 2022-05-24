f31840329d2044ca75065263c411969162f6f933
Vivek Shah <vivekkshah7@gmail.com>
2022-05-24 21:08:35 +0530

    #46: Add estuary storage flow fail test. - Handle flow exception.

--- protocolAggregator/estuaryStorage/src/test/kotlin/com/protocol180/aggregator/storage/flow/EstuaryStorageFlow.kt
+++ protocolAggregator/estuaryStorage/src/test/kotlin/com/protocol180/aggregator/storage/flow/EstuaryStorageFlow.kt
@@ -3,16 +3,13 @@ package com.protocol180.aggregator.storage.flow
 import co.paralleluniverse.fibers.Suspendable
 import com.protocol180.aggregator.storage.estuary.EstuaryStorageService
 import com.protocol180.aggregator.storage.utils.AESUtil
+import net.corda.core.flows.FlowException
 import net.corda.core.flows.FlowLogic
 import net.corda.core.flows.InitiatingFlow
 import net.corda.core.flows.StartableByRPC
 import net.corda.core.internal.readFully
 import net.corda.core.utilities.ProgressTracker
-import org.json.JSONArray
 import java.io.File
-import java.nio.file.Files
-import java.nio.file.Paths
-import javax.crypto.SecretKey
 
 
 /**
@@ -25,6 +22,7 @@ class EstuaryStorageFlow(private val token: String) : FlowLogic<Unit>() {
     override val progressTracker = ProgressTracker()
 
     @Suspendable
+    @Throws(EstuaryStorageFlowException::class)
     override fun call() {
         val estuaryStorageService = serviceHub.cordaService(EstuaryStorageService::class.java)
         val inputFile = ClassLoader.getSystemClassLoader().getResourceAsStream("Provider2InputData.csv").readFully();
@@ -34,10 +32,25 @@ class EstuaryStorageFlow(private val token: String) : FlowLogic<Unit>() {
         val decryptedFile = File("document.decrypted");
         AESUtil.encryptFile(key, ivParameterSpec, inputFile, encryptedFile);
         val uploadFile = File("document.encrypted");
-        val cid = estuaryStorageService.uploadContent(uploadFile, token);
-        val contents = estuaryStorageService.fetchContent(token);
-        estuaryStorageService.downloadFileFromEstuary(cid);
-        val downloadedFile = File("downloaded.encrypted");
-        AESUtil.decryptFile(key, ivParameterSpec, downloadedFile, decryptedFile);
+        var cid = "";
+        try {
+            cid = estuaryStorageService.uploadContent(uploadFile, token);
+        } catch (e: Exception) {
+            throw e.message?.let { EstuaryStorageFlowException(it) }!!;
+        }
+
+        try {
+            estuaryStorageService.downloadFileFromEstuary(cid);
+            val downloadedFile = File("downloaded.encrypted");
+            AESUtil.decryptFile(key, ivParameterSpec, downloadedFile, decryptedFile);
+        } catch (e: Exception) {
+            throw e.message?.let { EstuaryStorageFlowException(it) }!!;
+        }
     }
 }
+
+/**
+ * Thrown when the Estuary Storage Flow fails
+ */
+class EstuaryStorageFlowException(private val reason: String) :
+    FlowException("Estuary Storage Flow failed: $reason")
