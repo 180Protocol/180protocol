2fec3f4596073df3cc69e0a559be03dbbfc12ab7
Vivek Shah <vivekkshah7@gmail.com>
2022-06-08 19:56:15 +0530

    #46: Fix file path issue on estuary test case.

--- protocolAggregator/estuaryStorage/src/test/kotlin/com/protocol180/aggregator/storage/flow/EstuaryStorageFlowTest.kt
+++ protocolAggregator/estuaryStorage/src/test/kotlin/com/protocol180/aggregator/storage/flow/EstuaryStorageFlowTest.kt
@@ -1,13 +1,13 @@
 package com.protocol180.aggregator.storage.flow
 
+import net.corda.core.internal.readFully
 import net.corda.core.utilities.getOrThrow
 import net.corda.testing.node.*
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import org.junit.jupiter.api.AfterEach
 import java.io.File
-import java.nio.file.Files
-import java.nio.file.Paths
 import kotlin.test.assertFailsWith
 import kotlin.test.assertTrue
 
@@ -33,11 +33,21 @@ class EstuaryStorageFlowTest {
         network.stopNodes()
     }
 
+    @AfterEach
+    fun deleteFiles() {
+        val encryptedFile = File("src/test/resources/document.encrypted");
+        val decryptedFile = File("src/test/resources/document.decrypted");
+        val downloadedFile = File("downloaded.encrypted");
+        encryptedFile.delete();
+        decryptedFile.delete();
+        downloadedFile.delete();
+    }
+
     @Test
     fun estuaryStorageEncryptionDecryptionFlowFailTest() {
         val token = "ESTXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"; // Api key to authenticate estuary apis.
-        val updateFlow = EstuaryStorageFlow(token);
-        val future = consumer.startFlow(updateFlow);
+        val failFlow = EstuaryStorageFlow(token);
+        val future = consumer.startFlow(failFlow);
         assertFailsWith(EstuaryStorageFlowException::class) { future.getOrThrow() }
         network.runNetwork()
     }
@@ -45,9 +55,9 @@ class EstuaryStorageFlowTest {
     @Test
     fun estuaryStorageEncryptionDecryptionFlowTest() {
         val token = "ESTXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"; // Api key to authenticate estuary apis.
-        val updateFlow = EstuaryStorageFlow(token);
-        val future = consumer.startFlow(updateFlow);
-        val encoded: ByteArray = Files.readAllBytes(Paths.get(File("document.decrypted").path));
+        val successFlow = EstuaryStorageFlow(token);
+        val future = consumer.startFlow(successFlow);
+        val encoded: ByteArray = ClassLoader.getSystemClassLoader().getResourceAsStream("document.decrypted").readFully();
         assertTrue(encoded.isNotEmpty())
         println("Download file byte array: $encoded")
         network.runNetwork()
