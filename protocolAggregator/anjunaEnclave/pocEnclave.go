7c382033c67b4cc508cbea63b798278acc7b4273
hitesh <hiteshvpatel256@gmail.com>
2022-06-27 19:09:50 +0530

    Updated POC with configuration for consumer led flow utilising anjuna enclave for data transfer.

--- protocolAggregator/anjunaEnclave/pocEnclave.go
+++ protocolAggregator/anjunaEnclave/pocEnclave.go
@@ -3,16 +3,41 @@ package main
 import (
     "fmt"
     "io/ioutil"
+    "log"
+    "os"
 )
 
 func main() {
+
+    //reading data from provider's encrypted file.
+
     data, err := ioutil.ReadFile("/home/azureuser/anjuna-poc-host/greetings.txt.sealed")
     if err != nil {
         fmt.Println("File reading error", err)
         return
     }
     fmt.Println("Contents of file:", string(data))
-    //fmt.Println("!... Hello World ...!")
+    
+
+    
+
+    //writing reply to provider
+
+    f, err := os.Create("/home/azureuser/anjuna-poc-host/greetings_reply.txt.sealed")
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    defer f.Close()
+
+    _, err2 := f.WriteString("Thank you for providing greetings. \n")
+
+    if err2 != nil {
+        log.Fatal(err2)
+    }
+
+    fmt.Println("done")
 }
 
 
