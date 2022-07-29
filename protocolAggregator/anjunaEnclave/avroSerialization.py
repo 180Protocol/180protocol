8278650ecd830d5d93d6ee5f9569bc207dfcb09f
hitesh <hiteshvpatel256@gmail.com>
2022-07-29 12:29:30 +0530

    #31 POC for python avro deserialsation working in Anjuna-SGX environment

--- protocolAggregator/anjunaEnclave/avroSerialization.py
+++ protocolAggregator/anjunaEnclave/avroSerialization.py
@@ -10,18 +10,18 @@ inputSchema: avro.schema.RecordSchema = schema._props.get("fields")[0].props.get
 inputRecords = convertCsvToJson.make_json("ProviderInputData.csv")
 
 # print(convertCsvToJson.make_json("ProviderInputData.csv")[0])
-print(inputSchema)
+# print(inputSchema)
 
-writer = DataFileWriter(open("aggregateOutput.avro", "wb"), DatumWriter(), inputSchema)
-for record in inputRecords:
-    print(record)
-    writer.append(record)
+# writer = DataFileWriter(open("aggregateOutput.avro", "wb"), DatumWriter(), inputSchema)
+# for record in inputRecords:
+#     print(record)
+#     writer.append(record)
     
-# writer.append({"name": "Alyssa", "favorite_number": 256})
-# writer.append({"name": "Ben", "favorite_number": 7, "favorite_color": "red"})
-writer.close()
+# # writer.append({"name": "Alyssa", "favorite_number": 256})
+# # writer.append({"name": "Ben", "favorite_number": 7, "favorite_color": "red"})
+# writer.close()
 
-reader = DataFileReader(open("aggregateOutput.avro", "rb"), DatumReader())
+reader = DataFileReader(open("aggregateOutput.avro.encrypted", "rb"), DatumReader())
 for record in reader:
     print(record)
 
